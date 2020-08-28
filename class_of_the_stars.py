4aa52eda543297f44b092282eac95e6e0b15750f
Michael <michaelhinshaw@gmail.com>
2020-08-28 03:03:00 -0700

    class assignment

---
+++ class_of_the_stars.py
@@ -0,0 +1,18 @@
+
+
+class Celestial_Bodies
+    name = ' '
+    location = ' '
+    age = ' '
+    elemental_makeup
+
+class Planet(Celestial_Bodies)
+    distance_from_earth = ' '
+    distance_from_sun = ' '
+    moons = True
+
+class star(Celestial_Bodies)
+    label = ' ' #red giant, white dwarf, etc
+    size = ' '
+    mass = ' '
+    
