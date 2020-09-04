6609be80bf44094f40272ad57e388f18542f2fca
Michael <michaelhinshaw@gmail.com>
2020-09-04 12:33:01 -0700

    changes still not working

--- transferFilesModified.py
+++ transferFilesModified.py
@@ -2,6 +2,7 @@ import glob
 import os
 import datetime
 import shutil
+import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
@@ -15,17 +16,12 @@ class ParentWindow(Frame):
         self.master.geometry('{}x{}'.format(600, 400))
         self.master.title('Check for Modified Files')
         self.master.config(bg='lightgray')
+
         
 
-        self.labelFrame = ttk.LabelFrame(self, text = "Open File")
-        self.labelFrame.grid(column = 0, row = 1, padx = (20, 0), pady = (0, 20))
-
-        self.button()
         
-
-    def button(self):
-        self.button = ttk.Button(self.labelFrame, text = "Browse files", command = self.fileDialog)
-        self.button.grid(column = 1, row = 1)
+        self.btnBrowse = Button(self.master, text="Browse Files", width=10, height= 2, command = self.fileDialog)
+        self.btnSubmit.grid(row=2, column=1)
 
     def fileDialog(self):
         self.filename = filedialog.askopenfilename(initialdir = "/Users/Student/Desktop/Folder A", title = "Select a file", filetype= (("txt files", "*.txt"),("all files","*.*")))
@@ -35,29 +31,29 @@ class ParentWindow(Frame):
                         
                                                    
 
-def GetFileList(path, type):
-    '''
-    return a list of filename matching the given path and file type
-    '''
-    return glob.glob(path + "*" + type)
+    def GetFileList(path, type):
+        '''
+        return a list of filename matching the given path and file type
+        '''
+        return glob.glob(path + "*" + type)
 
-originPath = '/Users/Student/Desktop/Folder B/'
-destinationPath ='/Users/Student/Desktop/Folder A/'
-fileType= ".txt"
+    originPath = '/Users/Student/Desktop/Folder B/'
+    destinationPath ='/Users/Student/Desktop/Folder A/'
+    fileType= ".txt"
 
-fileList = GetFileList(originPath, fileType)
+    fileList = GetFileList(originPath, fileType)
 
-for file in fileList:
-    modifyDate = datetime.datetime.fromtimestamp(os.path.getmtime(file))
-    todaysDate = datetime.datetime.today()
+    for file in fileList:
+        modifyDate = datetime.datetime.fromtimestamp(os.path.getmtime(file))
+        todaysDate = datetime.datetime.today()
 
-    filePathList = file.split("\\")
-    filename = filePathList[-1]
+        filePathList = file.split("\\")
+        filename = filePathList[-1]
 
-    modifyDateLimit = modifyDate + datetime.timedelta(days=1)
+        modifyDateLimit = modifyDate + datetime.timedelta(days=1)
 
-    if modifyDateLimit > todaysDate:
-        shutil.copy2(file, destinationPath + filename)
+        if modifyDateLimit > todaysDate:
+            shutil.copy2(file, destinationPath + filename)
 
 if __name__ == "__main__":
     root = Tk()
