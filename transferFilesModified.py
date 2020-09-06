4c0287de2a20a2dcf3fd6b08b2b004a3cebc5718
Michael <michaelhinshaw@gmail.com>
2020-09-06 13:38:17 -0700

    completely lost

--- transferFilesModified.py
+++ transferFilesModified.py
@@ -9,35 +9,53 @@ from tkinter import filedialog
 
 class ParentWindow(Frame):
     def __init__(self,master):
-        Frame.__init__ (self,master)
+        Frame.__init__ (self)
 
         self.master = master
         self.master.resizable(width=True, height=True)
         self.master.geometry('{}x{}'.format(600, 400))
         self.master.title('Check for Modified Files')
         self.master.config(bg='lightgray')
-        
 
-        self.lbl = Label(self, text = "Open File", font=("Helvetica", 16), fg="black", bg="lightblue")
-        self.lbl.grid(column = 0, row = 1, padx = (20, 0), pady = (0, 20))
+ 
+        self.btnBrowseSrc = Button(self.master, text="Browse Source Files", width=30, height= 2, command = self.fileDialog1)
+        self.btnBrowseSrc.grid(row=2, column=1)
 
-        self.btnBrowse = Button(self.master, text="Browse Files", width=30, height= 2, command = self.fileDialog)
-        self.btnBrowse.grid(row=2, column=1)
+        self.btnBrowseDest = Button(self.master, text="Browse Destination Files", width=30, height= 2, command = self.fileDialog2)
+        self.btnBrowseDest.grid(row=4, column=1)
         
         self.btnGetFile = Button(self.master, text = "Copy Files", width=30, height = 2, command = self.GetFileList)
-        self.btnGetFile.grid(row=3, column=1)
+        self.btnGetFile.grid(row=6, column=1)
 
         self.btnCopied = Button(self.master, text="Copied Files",width=30, height = 2, command = self.copiedDialog)
-        self.btnCopied.grid(row=4, column=1)
+        self.btnCopied.grid(row=8, column=1)
 
-    def fileDialog(self):
-        self.filename = filedialog.askopenfilename(initialdir = "/Users/Student/Desktop/Folder A", title = "Select a file", filetype= (("txt files", "*.txt"),("all files","*.*")))
-        self.label = Label(self.label, text = "")
-        self.label.grid(column = 1, row = 2)
-        self.label.configure(text = self.filename)
+        self.src_path = tk.Entry(self.master,text=self.var_src, width=50, font=("Helvetica",14), fg="#333", bg="#fff")
+        self.src_path.grid(row=2, column = 2)
+
+        self.dest_path = tk.Entry(self.master,text=self.var_dest, width=50, font=("Helvetica",14), fg="#333", bg="#fff")
+        self.dest_path.grid(row=4, column=2)
+
+        
+
+
+        
+
+    def fileDialog1(self):
+        path1 = filedialog.askdirectory()
+        path1 = path1 +"/"
+        self.src_path.delete(0, END)
+        self.src_path.insert(END, path1)
+
+    def fileDialog2(self):
+        path2 = filedialog.askdirectory()
+        path2 = path2 + "/"
+        self.dest_path.delete(0, END)
+        self.dest_path.insert(END, path2)
+                
 
     def copiedDialog(self):
-        self.filename = filedialog.askopenfilename(initialdir = "/Users/Student/Desktop/Folder B", title = "Select a file", filetype = (("txt files", "*.txt"),("all files","*.*")))
+        self.filename = filedialog.askdirectory(initialdir = "/Users/Student/Desktop/Folder B", title = "Select a file", filetype = (("txt files", "*.txt"),("all files","*.*")))
         self.label2 = Label(self.label, text = "")
         self.label2.grid(column = 2, row = 2)
         self.label.configure(text = self.filename)
